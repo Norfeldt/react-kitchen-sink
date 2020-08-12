f2bb03f16e994a2357780eab392fa5703f2b2d41
Lasse Norfeldt <lasse@norfeldt.com>
2020-08-12 20:22:15 +0200

    add very basic photogrid

---
+++ src/components/PhotoGrid/index.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import PhotoGrid from '.'
+
+test('default renders 9 photos if provided 9', () => {
+  const photos = [...Array(10).keys()]
+  const { getAllByTestId } = render(<PhotoGrid photos={photos} />)
+
+  expect(getAllByTestId(PhotoGrid)).toHaveLength(9)
+})
+
+test('default renders n (n<9) photos n given', () => {
+  const photos = [...Array(3).keys()]
+  const { getAllByTestId } = render(<PhotoGrid photos={photos} />)
+
+  expect(getAllByTestId(PhotoGrid)).toHaveLength(photos.length)
+})
+
+test('default renders 9 photos if n (n>9) is given ', () => {
+  const photos = [...Array(11).keys()]
+  const { getAllByTestId } = render(<PhotoGrid photos={photos} />)
+
+  expect(getAllByTestId(PhotoGrid)).toHaveLength(9)
+})
+
+test('renders only 4 photos if number of photos is set to 4', () => {
+  const photos = [...Array(30).keys()]
+  const { getAllByTestId } = render(
+    <PhotoGrid photos={photos} numberOfPhotos={4} />
+  )
+
+  expect(getAllByTestId(PhotoGrid)).toHaveLength(4)
+})
