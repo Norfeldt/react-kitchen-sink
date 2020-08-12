a39166dab92b93661b64d5a6b82fc416700f17ad
Lasse Norfeldt <lasse@norfeldt.com>
2020-08-12 21:52:00 +0200

    implement masonry grid style for the images
    had to do rework to the styling
    locking the style with a snapshot

--- src/components/Photo/index.js
+++ src/components/Photo/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
-function Photo({ farm, server, id, secret }) {
-  const src = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_q.jpg`
-  return <img data-testid="flickr-grid-photo" src={src} />
+function Photo({ farm, server, id, secret, title }) {
+  const src = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_z.jpg`
+  return (
+    <img
+      width="300px"
+      style={styles.container}
+      data-testid="flickr-grid-photo"
+      src={src}
+      alt={title}
+    />
+  )
+}
+
+const styles = {
+  container: {
+    borderRadius: '1.5rem',
+  },
 }
 
 export default Photo
