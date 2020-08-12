f3d6a0b25451b64054a995d3f9de9ad545f200e8
Lasse Norfeldt <lasse@norfeldt.com>
2020-08-12 11:31:17 +0200

    add welcome component & clean boilerplate code from CRA

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import Welcome from './components/Welcome'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p style={styles.container}>
+      <Welcome />
+    </p>
+  )
 }
 
-export default App;
+const styles = {
+  container: {
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: '#85FFBD',
+    backgroundImage: 'linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%)',
+  },
+}
+
+export default App
