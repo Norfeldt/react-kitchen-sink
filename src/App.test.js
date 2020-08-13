6a7429590f3fe45a39549ba2547132fba0685b57
Lasse Norfeldt <lasse@norfeldt.com>
2020-08-13 22:29:40 +0200

    refactor test to use mock service worker instead of mocking axios
    this is the current recommened approach https://twitter.com/kentcdodds/status/1268544444858597378

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import { render, wait } from '@testing-library/react'
-import * as axiosMock from 'axios'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 
 import App from 'App'
+import { act } from 'react-dom/test-utils'
 
-jest.mock('axios')
-
-test('it calls Flickr REST request on initial load', async () => {
-  axiosMock.get.mockImplementation(() =>
-    Promise.resolve({ data: { photos: { photo: [1, 2, 3] } } })
+const fakeServer = setupServer(
+  rest.get(
+    'https://api.flickr.com/services/rest/?method=flickr.photos.search',
+    (req, res, ctx) =>
+      res(ctx.status(200), ctx.json({ photos: { photo: [1, 2, 3] } }))
   )
-  render(<App />)
+)
 
+beforeAll(() => fakeServer.listen())
+afterEach(() => fakeServer.resetHandlers())
+afterAll(() => fakeServer.close())
+
+test('it calls Flickr REST request on initial load to show some photos', async () => {
+  const { getAllByTestId } = render(<App />)
+
+  await act(async () => {})
   await wait()
 
-  expect(axiosMock.get).toHaveBeenCalledTimes(1)
-  expect(axiosMock.get).toHaveBeenCalledWith(
-    `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.REACT_APP_API_KEY}&per_page=10&format=json&nojsoncallback=1'&text=planning`
-  )
+  expect(getAllByTestId(PHOTO_COMP_TEST_ID)).toHaveLength(3)
 })
