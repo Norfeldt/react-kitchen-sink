6643b837e0de7944d0eab20a7c6d98fb030ef893
Lasse Norfeldt <lasse@norfeldt.com>
2020-08-13 22:28:22 +0200

    fix wired side-effect that the toMatchInlineSnapshot
    - it's a bit wierd that passing the PhotoGrid as TestId worked.. But that have been changed now to use the constants

--- src/components/PhotoGrid/index.test.js
+++ src/components/PhotoGrid/index.test.js
@@ -1,41 +1,41 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React from 'react'
+import { render } from '@testing-library/react'
 
-import PhotoGrid from ".";
+import PhotoGrid from '.'
 
-test("default renders 9 photos if provided 9", () => {
-  const photos = [...Array(10).keys()];
-  const { getAllByTestId } = render(<PhotoGrid photos={photos} />);
+test('default renders 9 photos if provided 9', () => {
+  const photos = [...Array(10).keys()]
+  const { getAllByTestId } = render(<PhotoGrid photos={photos} />)
 
-  expect(getAllByTestId(PhotoGrid)).toHaveLength(9);
-});
+  expect(getAllByTestId(PHOTO_COMP_TEST_ID)).toHaveLength(9)
+})
 
-test("default renders n (n<9) photos n given", () => {
-  const photos = [...Array(3).keys()];
-  const { getAllByTestId } = render(<PhotoGrid photos={photos} />);
+test('default renders n (n<9) photos n given', () => {
+  const photos = [...Array(3).keys()]
+  const { getAllByTestId } = render(<PhotoGrid photos={photos} />)
 
-  expect(getAllByTestId(PhotoGrid)).toHaveLength(photos.length);
-});
+  expect(getAllByTestId(PHOTO_COMP_TEST_ID)).toHaveLength(photos.length)
+})
 
-test("default renders 9 photos if n (n>9) is given ", () => {
-  const photos = [...Array(11).keys()];
-  const { getAllByTestId } = render(<PhotoGrid photos={photos} />);
+test('default renders 9 photos if n (n>9) is given ', () => {
+  const photos = [...Array(11).keys()]
+  const { getAllByTestId } = render(<PhotoGrid photos={photos} />)
 
-  expect(getAllByTestId(PhotoGrid)).toHaveLength(9);
-});
+  expect(getAllByTestId(PHOTO_COMP_TEST_ID)).toHaveLength(9)
+})
 
-test("renders only 4 photos if number of photos is set to 4", () => {
-  const photos = [...Array(30).keys()];
+test('renders only 4 photos if number of photos is set to 4', () => {
+  const photos = [...Array(30).keys()]
   const { getAllByTestId } = render(
     <PhotoGrid photos={photos} numberOfPhotos={4} />
-  );
+  )
 
-  expect(getAllByTestId(PhotoGrid)).toHaveLength(4);
-});
+  expect(getAllByTestId(PHOTO_COMP_TEST_ID)).toHaveLength(4)
+})
 
-test("renders with masonry grid style", () => {
-  const photos = [...Array(9).keys()];
-  const { container } = render(<PhotoGrid photos={photos} />);
+test('renders with masonry grid style', () => {
+  const photos = [...Array(9).keys()]
+  const { container } = render(<PhotoGrid photos={photos} />)
 
   expect(container).toMatchInlineSnapshot(`
     <div>
@@ -149,5 +149,5 @@ test("renders with masonry grid style", () => {
         </div>
       </div>
     </div>
-  `);
-});
+  `)
+})
