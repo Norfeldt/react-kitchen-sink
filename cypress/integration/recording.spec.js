8dab5f9d972dc2a9666f957899dca2e2b4ca9a1d
Lasse Norfeldt <lasse@norfeldt.com>
2021-03-01 10:58:12 +0100

    add a file to demonstrate the 'macro' recording

---
+++ cypress/integration/recording.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+// ☝️ magic 🎩 ✨ https://youtu.be/lgurVvQsOTY?t=1225 ✨ - https://www.typescriptlang.org/docs/handbook/triple-slash-directives.html
+
+context('Experimenting with recording', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('allows to easy record a test', () => {
+    // Record something
+  })
+})
