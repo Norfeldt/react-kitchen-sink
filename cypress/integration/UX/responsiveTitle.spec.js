9f03557109decdc614725709b4cf7467630c0224
Lasse Norfeldt <ln@abtion.com>
2021-03-02 09:39:18 +0100

    refactor to improve readability

--- cypress/integration/UX/responsiveTitle.spec.js
+++ cypress/integration/UX/responsiveTitle.spec.js
@@ -9,24 +9,26 @@ context('Responsive title', () => {
   // https://on.cypress.io/interacting-with-elements
 
   it('adjust dynamically to viewport', () => {
-    const height = 480
-    let width = 501
-
     const viewportName = (description) => {
       const dimensions = `${width}px x ${height}px`
       return description ? `${dimensions} - ${description}` : dimensions
     }
 
+    const height = 480
+    let width = 501
     cy.viewport(width, height)
       .get('h1')
       .contains(/search good old flickr/i)
+
     cy.findByTestId('verb').contains(/search/i)
     cy.findByTestId('adjectives').contains(/good old/i)
     cy.findByTestId('noun').contains(/flickr/i)
-    cy.screenshot(viewportName(), { blackout: ['[aria-label="search emoji"]'] })
-    cy.screenshot(`${viewportName()} - cencured`, {
-      blackout: ['[aria-label="search emoji"]'],
+
+    cy.screenshot(viewportName())
+    cy.screenshot(`${viewportName()} - blackout`, {
+      blackout: ['h1'],
     })
+
     cy.get('h1').screenshot(viewportName('h1'))
 
     cy.findByTestId('verb').should('be.visible')
@@ -35,17 +37,21 @@ context('Responsive title', () => {
 
     width = 500
     cy.viewport(width, height)
+
     cy.findByTestId('verb').should('be.visible')
     cy.findByTestId('adjectives').should('not.be.visible')
     cy.findByTestId('noun').should('be.visible')
+
     cy.screenshot(viewportName())
     cy.get('h1').screenshot(viewportName('h1'))
 
     width = 200
     cy.viewport(width, height)
+
     cy.findByTestId('verb').should('not.be.visible')
     cy.findByTestId('adjectives').should('not.be.visible')
     cy.findByTestId('noun').should('be.visible')
+
     cy.screenshot(viewportName())
     cy.get('h1').screenshot(viewportName('h1'))
   })
