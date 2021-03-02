439f4514b88b099bca01bce747e7f9e670c249cd
Lasse Norfeldt <ln@abtion.com>
2021-03-02 14:03:18 +0100

    add sucess and failure tests to searching

---
+++ cypress/integration/Behavior/searchingFlickr.spec.js
@@ -0,0 +1,62 @@
+/// <reference types="cypress" />
+// ☝️ magic 🎩 ✨ https://youtu.be/lgurVvQsOTY?t=1225 ✨ - https://www.typescriptlang.org/docs/handbook/triple-slash-directives.html
+import { photosResponse as photosResponseFake } from '@fixtures/fakeResponses'
+
+const url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search'
+
+describe('Main page search results', () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://farm*.staticflickr.com/**/*',
+      },
+      { fixture: '1080x608.jpg' }
+    ).as('getPhotos')
+
+    cy.visit('http://localhost:3000')
+  })
+
+  describe('Sucess', () => {
+    it('contains a grid of search result photos', () => {
+      cy.intercept(
+        {
+          method: 'GET',
+          url,
+        },
+        {
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: {
+            ...photosResponseFake(),
+          },
+          stat: 'ok',
+        }
+      )
+
+      cy.get('input').type('cats{enter}')
+
+      // Make sure all requests are completed before proceeding
+      //https://docs.cypress.io/guides/guides/network-requests.html#Waiting
+      cy.wait([...new Array(9)].map((e) => '@getPhotos')) // https://github.com/cypress-io/cypress/issues/4389
+
+      cy.findAllByTestId('flickr-grid-photo').should('have.length', 9)
+
+      cy.findByText('Oops! Something went wrong..').should('be.hidden')
+    })
+  })
+
+  describe('Failure', () => {
+    it('throws the error', () => {
+      cy.intercept('GET', url, { forceNetworkError: true }).as('err')
+
+      cy.get('input').type('cats{enter}')
+
+      // assert that this request happened, and that it ended in an error
+      cy.wait('@err').should('have.property', 'error')
+
+      cy.findByText('Oops! Something went wrong..').should('be.visible')
+    })
+  })
+})
