66ce63014fdf4b9f7c498f1434fb44a54dbe2df6
Lasse Norfeldt <ln@abtion.com>
2021-03-02 15:49:34 +0100

    fix initial fetching of photos

--- cypress/integration/Behavior/searchingFlickr.spec.js
+++ cypress/integration/Behavior/searchingFlickr.spec.js
@@ -6,6 +6,43 @@ const url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search'
 
 describe('Main page search results', () => {
   beforeEach(() => {
+    // initial fetching of "planning" photos
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /^https:\/\/api\.flickr\.com\/services\/.*text=planning/,
+      },
+      {
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: {
+          ...photosResponseFake(),
+        },
+        stat: 'ok',
+      }
+    )
+    // searching for "cats"
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /^https:\/\/api\.flickr\.com\/services\/.*text=cats/,
+      },
+      {
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: {
+          ...photosResponseFake(3),
+        },
+        stat: 'ok',
+      }
+    )
+    // searching for "failure"
+    cy.intercept('GET', /^https:\/\/api\.flickr\.com\/services\/.*text=failure/, {
+      forceNetworkError: true,
+    }).as('err')
+
     cy.intercept(
       {
         method: 'GET',
@@ -15,47 +52,34 @@ describe('Main page search results', () => {
     ).as('getPhotos')
 
     cy.visit('http://localhost:3000')
+
+    // Make sure all requests are completed before proceeding
+    //https://docs.cypress.io/guides/guides/network-requests.html#Waiting
+    cy.wait([...new Array(9)].map((e) => '@getPhotos')) // https://github.com/cypress-io/cypress/issues/4389
+    cy.findAllByTestId('flickr-grid-photo').should('have.length', 9)
   })
 
-  describe('Sucess', () => {
+  describe('Success', () => {
     it('contains a grid of search result photos', () => {
-      cy.intercept(
-        {
-          method: 'GET',
-          url,
-        },
-        {
-          headers: {
-            'content-type': 'application/json',
-          },
-          body: {
-            ...photosResponseFake(),
-          },
-          stat: 'ok',
-        }
-      )
-
       cy.get('input').type('cats{enter}')
 
-      // Make sure all requests are completed before proceeding
-      //https://docs.cypress.io/guides/guides/network-requests.html#Waiting
-      cy.wait([...new Array(9)].map((e) => '@getPhotos')) // https://github.com/cypress-io/cypress/issues/4389
+      cy.wait([...new Array(3)].map((e) => '@getPhotos'))
 
-      cy.findAllByTestId('flickr-grid-photo').should('have.length', 9)
+      cy.findAllByTestId('flickr-grid-photo').should('have.length', 3)
 
       cy.findByText('Oops! Something went wrong..').should('be.hidden')
     })
   })
 
-  describe('Failure', () => {
-    it('throws the error', () => {
-      cy.intercept('GET', url, { forceNetworkError: true }).as('err')
-
-      cy.get('input').type('cats{enter}')
+  describe('Failure - response throws an error', () => {
+    it('displays an error dialog', () => {
+      cy.get('input').type('failure{enter}')
 
       // assert that this request happened, and that it ended in an error
       cy.wait('@err').should('have.property', 'error')
 
+      // Still showing the intial "planning" photos
+      cy.findAllByTestId('flickr-grid-photo').should('have.length', 9)
       cy.findByText('Oops! Something went wrong..').should('be.visible')
     })
   })
